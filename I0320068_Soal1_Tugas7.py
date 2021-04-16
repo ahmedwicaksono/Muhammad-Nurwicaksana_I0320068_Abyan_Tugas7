ea1365999b50ee0a20a2f800d411fd91a0d080a0
Muhammad Nurwicaksana <ahmed.wicaksono16@gmail.com>
2021-04-17 02:35:56 +0700

    memed

---
+++ I0320068_Soal1_Tugas7.py
@@ -0,0 +1,15 @@
+# Metode center
+Judul = "Survei Pengalaman agrowisata"
+a = Judul.center (50,'=')
+print (a)
+
+# Metode endswith
+str = "Apakah tempatnya ramai?"
+print("Apakah tempatnya ramai?")
+print (str.endswith("ramai?"))
+
+#Metode replace dan endswith
+str = "Apakah Buahnya banyak?"
+b = str.replace ("banyak?","berlimpah?")
+print(b)
+print(str.endswith("banyak?"))
---
+++ I0320068_Soal2_Tugas7.py
@@ -0,0 +1,19 @@
+# Fungsi floor
+import math
+nilai = 90
+print ("Ahmed mendapatkan nilai fisika sebesar", math.floor(nilai), "pada ujian akhir semester 1")
+
+# Fungsi min dan max
+import math
+Biologi = 90
+Kalkulus = 97
+Prokom = 75
+print ("Nilai tertinggi yang Ahmed dapatkan pada semester ini ialah ", max(Biologi,Kalkulus,Prokom), ", yaitu pada matkul Kalkulus")
+print ("Nilai terendah yang Ahmed dapatkan pada semester ini ialah ", min(Biologi,Kalkulus,Prokom), ", yaitu pada matkul Prokom")
+
+# Fungsi sqrt
+import math
+Alas = 60
+Tinggi = 60
+S_miring = Alas ** 2 + Tinggi ** 2
+print ("Ahmed ditugaskan oleh Pak Agung untuk menghitung sisi miring lapangan ekonomi, \nhasil pengamatannya menyatakan bahwa besar sisi miring lapangan ekonomi ialah",math.sqrt(S_miring),"m")
